218e64f828be4e8e50669c878cd82aa81ecdc18f
subhash131 <subhashnayak131@gmail.com>
2023-09-13 03:41:44 +0530

    scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,93 @@
 import { motion, useScroll, useTransform } from "framer-motion"
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import Game from "./components/Game"
 import Aim from "./components/Aim"
+import Reveal from "./components/Reveal"
+import { ChevronDown } from "lucide-react"
+// import {
+// 	Link,
+// 	Button,
+// 	Element,
+// 	Events,
+// 	animateScroll as scroll,
+// 	scrollSpy,
+// 	scroller,
+// } from "react-scroll"
 
 function App() {
-	const motionRef = useRef<HTMLDivElement>(null)
 	const cursorRef = useRef<HTMLDivElement>(null)
-	const titleRef = useRef<HTMLDivElement>(null)
+	const section1Ref = useRef<HTMLDivElement>(null)
+	const section2Ref = useRef<HTMLDivElement>(null)
+	const section3Ref = useRef<HTMLDivElement>(null)
+	const wrapRef = useRef<HTMLDivElement>(null)
+	const [activeSection, setActiveSection] = useState("section1")
+
 	useEffect(() => {
-		motionRef.current?.addEventListener("mousemove", (e) => {
+		section1Ref.current?.addEventListener("mousemove", (e) => {
 			cursorRef.current!.style.left = `${e.clientX - 20}px`
 			cursorRef.current!.style.top = `${e.clientY - 20}px`
 		})
+		var lastScrollTop = 0
+		console.log(document.getElementById("section2")!.offsetTop)
+
+		document.onscroll = () => {
+			// if (activeSection === "section1") {
+			var scrollTop =
+				window.pageYOffset || document.documentElement.scrollTop
+			if (scrollTop > lastScrollTop) {
+				window.scrollTo({
+					top: section2Ref.current!.offsetTop,
+					behavior: "smooth",
+				})
+			} else if (scrollTop < lastScrollTop) {
+				window.scrollTo({
+					top: section1Ref.current!.offsetTop,
+					behavior: "smooth",
+				})
+			}
+			lastScrollTop = scrollTop <= 0 ? 0 : scrollTop
+			setActiveSection("section2")
+			// }
+		}
 	})
+	const handleScroll = (elemRef: React.RefObject<HTMLDivElement>) => {
+		window.scrollTo({ top: elemRef.current!.offsetTop, behavior: "smooth" })
+	}
 
 	const { scrollYProgress } = useScroll({
-		target: motionRef,
+		target: section2Ref,
 	})
 	const x = useTransform(scrollYProgress, [0, 1], ["55.5%", "-95%"])
 	return (
-		<div className='relative' ref={motionRef}>
-			<div className='h-[100vh]  bg-red-00 grid place-items-center relative z-100 cursor-none'>
-				<div
-					className='text-center  select-none backdrop-blur-sm p-4 rounded w-full h-full grid place-items-center'
-					ref={titleRef}
-				>
-					<div>
-						<p className='font-cursive text-8xl p-2	'>
-							Subhash nayak
-						</p>
-						<p className='text-4xl font-cursive'>
-							Front-End Developer
-						</p>
+		<div className='relative' ref={wrapRef}>
+			<div
+				className='h-[100vh]  bg-red-00 grid place-items-center relative z-100 cursor-none'
+				ref={section1Ref}
+				id='section1'
+			>
+				<div className='select-none backdrop-blur-sm p-4 rounded w-full h-full grid place-items-center'>
+					<div className='items-center flex flex-col gap-2'>
+						<Reveal
+							variants={{
+								hidden: { left: 0, visibility: "hidden" },
+								visible: { left: 0, visibility: "hidden" },
+							}}
+						>
+							<p className='font-cursive text-8xl p-2	'>
+								Subhash nayak
+							</p>
+						</Reveal>
+						<Reveal
+							variants={{
+								hidden: { left: 0, visibility: "hidden" },
+								visible: { left: 0, visibility: "hidden" },
+							}}
+							delay={0.8}
+						>
+							<p className='text-4xl font-cursive p-2'>
+								Front-End Developer
+							</p>
+						</Reveal>
 					</div>
 				</div>
 				<div
@@ -40,10 +96,27 @@ function App() {
 				>
 					<Aim />
 				</div>
+				<div className='absolute bottom-10'>
+					<button
+						onClick={() => handleScroll(section2Ref)}
+						onMouseMove={() => {
+							cursorRef.current!.style.visibility = "hidden"
+						}}
+						onMouseLeave={() => {
+							cursorRef.current!.style.visibility = "visible"
+						}}
+					>
+						<ChevronDown />
+					</button>
+				</div>
 
 				<Game />
 			</div>
-			<div className='h-[400vh] w-full bg-black relative'>
+			<div
+				className='h-[400vh] w-full bg-black relative'
+				id='section2'
+				ref={section2Ref}
+			>
 				<div className='h-screen bg-blue-600 sticky top-0 items-center overflow-hidden flex justify-center'>
 					{" "}
 					<motion.div className='flex' style={{ x }}>
---
+++ src/components/Reveal.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect } from "react"
+import { motion, useAnimation, useInView } from "framer-motion"
+type Props = {
+	children: JSX.Element
+	width?: "fit-content" | "100%"
+	variants?: {
+		hidden: any
+		visible: any
+	}
+	delay?: number
+}
+
+const Reveal = ({
+	children,
+	width = "fit-content",
+	variants = {
+		hidden: { right: 0 },
+		visible: { right: "100%" },
+	},
+	delay = 0.5,
+}: Props) => {
+	const containerRef = React.useRef<HTMLDivElement>(null)
+	const isInView = useInView(containerRef, { once: true })
+	const mainControls = useAnimation()
+	const slideControls = useAnimation()
+	useEffect(() => {
+		if (isInView) {
+			mainControls.start("visible")
+			slideControls.start("visible")
+		}
+	})
+	return (
+		<div
+			className='relative overflow-hidden'
+			style={{ width }}
+			ref={containerRef}
+		>
+			<motion.div
+				variants={{
+					hidden: { opacity: 0, y: 75 },
+					visible: { opacity: 1, y: 0 },
+				}}
+				initial='hidden'
+				animate={mainControls}
+				transition={{ duration: 0.5, ease: "easeInOut", delay }}
+			>
+				{children}
+			</motion.div>
+			<motion.div
+				variants={variants}
+				initial='hidden'
+				animate={slideControls}
+				transition={{ duration: 0.3, ease: "easeIn" }}
+				className='absolute top-0 bottom-0 left-0 right-0 bg-gray-900 z-20'
+			></motion.div>
+		</div>
+	)
+}
+
+export default Reveal
